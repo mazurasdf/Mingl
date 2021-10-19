bf0134de7c22a971c2c407ec660d538f07445ac6
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-10-19 15:09:55 -0500

    Todd's Login Reg Starter

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,23 +5,103 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 using Mingl.Models;
 
-namespace Mingl.Controllers
+namespace Mingl.Contollers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
         private MyContext _context;
 
-        public HomeController(MyContext context)
+        public HomeController(ILogger<HomeController> logger, MyContext context)
         {
+            _logger = logger;
             _context = context;
         }
 
         public IActionResult Index()
         {
+            return View("LoginReg");
+        }
+
+        [HttpPost("RegisterUser")]
+        public IActionResult RegisterUser(User newUser)
+        {
+            if(ModelState.IsValid)
+            {
+                if(_context.Users.Any(user => user.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "Email already in system!");
+                    return View("LoginReg");
+                }
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+
+                _context.Add(newUser);
+                _context.SaveChanges();
+
+                HttpContext.Session.SetInt32("LoggedUserId", newUser.UserId);
+
+
+                return RedirectToAction("dashboard");
+            }
+            return View("LoginReg");
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginUser checkMe)
+        {
+            if(ModelState.IsValid)
+            {
+                User userInDb = _context.Users.FirstOrDefault(use => use.Email == checkMe.LoginEmail);
+                if(userInDb == null)
+                {
+                    ModelState.AddModelError("LoginEmail", "Invalid Login!");
+                    return View("LoginReg");
+                }
+
+                PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
+
+                var result = hasher.VerifyHashedPassword(checkMe, userInDb.Password, checkMe.LoginPassword);
+
+                if(result ==0)
+                {
+                    ModelState.AddModelError("LoginEmail", "Invalid Login!");
+                    return View("LoginReg");
+                }
+
+                HttpContext.Session.SetInt32("LoggedUserId", userInDb.UserId);
+                return RedirectToAction("dashboard");
+            }
+            return View("LoginReg");
+        }
+
+        [HttpGet("/dashboard")]
+        public IActionResult Dashboard()
+        {
+            int? loggedUserId = HttpContext.Session.GetInt32("LoggedUserId");
+            if(loggedUserId==null) return RedirectToAction("Index");
+
+            ViewBag.User = _context.Users.FirstOrDefault(use => use.UserId == loggedUserId);
+
+
             return View();
         }
+
+        
+
+        [HttpGet("logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
 
         public IActionResult Privacy()
         {
