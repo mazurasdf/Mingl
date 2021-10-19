da14f813c4667ce22fd8e53ead941897f80e75ed
mazurasdf <mazurasdf@gmail.com>
2021-10-19 16:08:50 -0500

    beginning to get basic data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Mingl.Models;
 
@@ -21,6 +23,45 @@ namespace Mingl.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet("matching")]
+        public IActionResult MatchingMain()
+        {
+            //TODO testing with session, remove this later
+            // HttpContext.Session.GetInt32("LoggedUserId");
+            int loggedUserId = 6;
+
+            //viewbag logged user
+            ViewBag.LoggedUser = _context.Users.FirstOrDefault(user => user.UserId == loggedUserId);
+            
+            //viewbag users who have sent a match request
+            ViewBag.ReceivedRequests = _context.MatchRequests
+                .Include(mr => mr.Sender)
+                .Where(mr => mr.ReceiverId == loggedUserId)
+                .ToList();
+            
+            //search for random user who is looking for gender that logged user identifies as
+            int OthersPrefer =   ViewBag.LoggedUser.Gender == "Female" ? 2
+                                : ViewBag.LoggedUser.Gender == "Male" ? 3
+                                : 1;
+            string UserPrefers =    ViewBag.LoggedUser.PreferredUsers == 2 ? "Female"
+                                    : ViewBag.LoggedUser.PreferredUsers == 3 ? "Male"
+                                    : "Any";
+
+            var RandoList = _context.Users
+                .Where(user => user.UserId != loggedUserId)
+                .Where(user => user.PreferredUsers == OthersPrefer)
+                .Where(user => UserPrefers == "Any" || user.Gender == UserPrefers);
+            Random rand = new Random();
+            int toSkip = rand.Next(RandoList.Count());
+            //viewbag first random user who could match with logged user
+            ViewBag.RandomUser = RandoList
+                .Skip(toSkip)
+                .FirstOrDefault();
+
+            return View();
+
         }
 
         public IActionResult Privacy()
